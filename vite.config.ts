bc669471642890a10f9e3badc42028841723ed94
MacvilleFernandes <macferns27@gmail.com>
2023-10-26 15:20:01 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './resume-project-mac'
+
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/resume-project-mac/'
+  }
+
+  return config
 })
